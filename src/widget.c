b9f5d2a8a93dd5ab16785cfc5299ec31d49ca77f
liuhuoz <liuhuo2012@gmail.com>
2026-05-22 00:21:37 +0800

    def M_PI as float instead of double

--- src/widget.c
+++ src/widget.c
@@ -8,7 +8,7 @@
 
 // Define M_PI if not available in embedded environment
 #ifndef M_PI
-#define M_PI 3.14159265358979323846
+#define M_PI 3.14159265358979323846f
 #endif
 
 #include <zmk/battery.h>
